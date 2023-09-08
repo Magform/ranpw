6bbda155d13f1b076d3c4cabbc3adf38275b19c1
Magform <nicolas25.02.2003.nf@gmail.com>
2023-09-09 00:26:26 +0200

    Added Credits

---
+++ include/Credits.h
@@ -0,0 +1,19 @@
+#ifndef Credits_H_
+#define Credits_H_
+
+
+class Credits {
+public:
+    char* getName() {
+        return authorName;
+    }
+    char* getAlias() {
+        return authorAlias;
+    }
+
+private:
+    char* authorName = "Nicolas Ferraresso";
+    char* authorAlias = "Magform";
+};
+
+#endif //Credits_H_
