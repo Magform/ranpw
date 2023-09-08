559d92bb41760ae99c53b2c4fd44e640e24414e4
Magform <nicolas25.02.2003.nf@gmail.com>
2023-09-09 00:25:54 +0200

    Added 5 more health

--- lib/Kraken/Kraken.cpp
+++ lib/Kraken/Kraken.cpp
@@ -5,8 +5,8 @@
 #include "Kraken.h"
 
 Kraken::Kraken() {
-    health = 20;
-    hunger = 20;
+    health = 25;
+    hunger = 25;
 }
 
 int Kraken::getHealth(){
@@ -28,30 +28,33 @@ void Kraken::live(){
     while(1){
 
         if(checkNumber == minutesHunger){
-            checkNumber == 0;
+            checkNumber = 0;
             minutesHunger = rand() % 5 + 1;
             int hungerLoss = rand() % 5 + 1;
             hunger -= hungerLoss;
             if(hunger <= 0){
                 int healthLoss = rand() % 3 + 1;
                 health -= healthLoss;
+                if(health <= 0){
+                    //Death (to be configured...)
+                }
             }
         }
 
-        if (hunger >= 15) {
+        if (hunger >= 20) {
             health += 1;
         }
 
-        // Ensure hunger and health values stay within bounds (0-20)
-        hunger = std::max(0, std::min(20, hunger));
-        health = std::max(0, std::min(20, health));
+        // Ensure hunger and health values stay within bounds (0-25)
+        hunger = std::max(0, std::min(25, hunger));
+        health = std::max(0, std::min(25, health));
 
         checkNumber++;
-        ThisThread::sleep_for(1s);
+        ThisThread::sleep_for(6s);
     }
 }
 
 int Kraken::addHunger(int hunger){
     this->hunger += hunger;
-    hunger = std::max(0, std::min(20, hunger));
+    hunger = std::max(0, std::min(25, hunger));
 }
